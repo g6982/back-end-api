dbe0dfbeaf605a3edb2dfbbb36dd9453525da6b0
Kadweka Katana <Kadweka>
2022-11-16 06:40:59 +0300

    update changes

--- odoo-custom-addons/pos_laundry_service/models/res_partner.py
+++
@@ -1,7 +0,0 @@
-from odoo import models,fields,_,api,tools
-
-class ResPartner(models.Model):
-    
-    _inherit = 'res.partner'
-    
-    gender = fields.Selection([('male','Male'), ('female','Female')],string ="Gender")
